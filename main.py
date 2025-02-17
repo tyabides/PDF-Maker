145ff1d980cdc252ff5dc5798be46d6c7daeda0e
tylerjohnson <tyjohnson77@gmail.com>
2025-02-17 09:02:32 -0800

    Format Revision

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from fpdf import FPDF
 import pandas as pd
 
+
 pdf = FPDF(orientation="P", unit="mm", format="A4")
 pdf.set_auto_page_break(auto=False, margin=0)
 
@@ -28,8 +29,6 @@ for index, row in df.iterrows():
         pdf.set_font(family="Times", style="I", size=8)
         pdf.set_text_color(24, 28, 33)
         pdf.cell(w=0, h=10, txt=row["Topic"], align="R")
-
-
 
 
 pdf.output("output.pdf")
